9bfd74991491dac6cb8b50169cc5625a7a2fb269
Hugo Lapa <Hugolapa888@gmail.com>
2026-01-10 21:08:21 +0000

    Add Terraform configuration files

---
+++ terraform/variables.tf
@@ -0,0 +1,13 @@
+variable "project_id" {
+  description = "The GCP project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "The GCP region for resources"
+  type        = string
+  default     = "us-central1"
+}
+
+
+
---
+++ terraform/versions.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 7.14"
+    }
+  }
+
+  backend "gcs" {
+    bucket = "prom-grafana-statefiles"
+    prefix = "."
+  }
+
+}
